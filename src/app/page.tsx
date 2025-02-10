dc6e60df98374969277f9ed30ae2d850b78fa947
AOM-Technologie <glgharib@gmail.com>
2025-02-10 20:33:39 +0100

    Redimentionnement des videos et responsivité
    Signed-off-by: AOM-Technologie <glgharib@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ const Page: React.FC = () => {
         />
       </div>
 
-      <section className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center mt-20">
+      <section className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center mt-20">
         {videos.map((video, index) => (
           <div className="video-card" key={index}>
             <VideoCard
